9ec370fd1e4d7db3c24a66b50253c9df1119fb1b
Stephen H. Gerstacker <stephen@airsquirrels.com>
2022-09-09 09:49:57 -0400

    Initial Commit

---
+++ PDF Test/PDF_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_TestApp.swift
+//  PDF Test
+//
+//  Created by Stephen Gerstacker on 2022-09-09.
+//
+
+import SwiftUI
+
+@main
+struct PDF_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
